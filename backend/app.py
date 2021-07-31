eadfe927849841270d9954ae32f656603d2c4f1f
memset0 <34177126+memset0@users.noreply.github.com>
2021-07-31 21:01:25 +0800

    feat: handle test data

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import flask
 import shutil
 import random
 from os import path
+from pprint import pprint
 from flask import Flask, request
 
 import image
@@ -12,22 +13,40 @@ from ocr import BaiduOCR
 
 dirname = path.dirname(__file__)
 
+# 配置文件
 full_config = configure.load()
 config = full_config['backend']
-
-app = Flask(__name__)
-ocr = BaiduOCR(config)
+MODE = full_config['mode']
 
 # 数据
 data = config['test_data'] if 'test_data' in config else {}
 
+# Flask App
+app = Flask(__name__)
+app.config['mode'] = MODE
+
+# OCR
+ocr = BaiduOCR(config['baidu_aip']['app_id'],
+               config['baidu_aip']['api_key'],
+               config['baidu_aip']['secret_key'])
+
 # 临时文件上传目录
 upload_folder = path.abspath(path.join(dirname, '..', config['upload_folder']))
 app.config['upload_folder'] = upload_folder
-if path.exists(upload_folder) and config['mode'] != 'development':
+if path.exists(upload_folder) and MODE != 'development':
     shutil.rmtree(upload_folder)
 if not path.exists(upload_folder):
     os.mkdir(upload_folder)
+
+
+# 图片处理
+def handleImage(id, cutter=[]):
+    dir = path.join(app.config['upload_folder'], id)
+    image.transform(path.join(dir, 'source.jpg'),
+                    path.join(dir, 'problem.jpg'),
+                    vec2=cutter)
+    text = ocr.scan(path.join(dir, 'problem.jpg'))
+    return {'text': text, 'type': 'image'}
 
 
 # 上传图片
@@ -38,10 +57,7 @@ def uploadImage():
     dist_dir = path.join(app.config['upload_folder'], id)
     os.mkdir(dist_dir)
     file.save(path.join(dist_dir, 'source.jpg'))
-    image.transform(path.join(dist_dir, 'source.jpg'),
-                    path.join(dist_dir, 'problem.jpg'))
-    text = ocr.scan(path.join(dist_dir, 'problem.jpg'))
-    data[id] = {'text': text, 'type': 'image'}
+    data[id] = handleImage(dist_dir)
     return flask.jsonify({'code': 200, 'id': id, 'data': data[id]})
 
 
@@ -68,7 +84,12 @@ def search(id):
     return flask.jsonify(data[id]['result'])
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',
-            port=config['port'],
-            debug=(full_config['mode'] == 'development'))
+if __name__ == '__main__':
+    if MODE == 'development':
+        for id in config['test_data']:
+            it = config['test_data'][id]
+            if it['type'] == 'image' and 'text' not in it:
+                cutter = it['cutter'] if 'cutter' in it else []
+                data[id] = handleImage(id, cutter)
+        pprint(data)
+    app.run(host='0.0.0.0', port=config['port'], debug=(MODE == 'development'))

9846f3783ed56c3d874a44641296569c9739579d
memset0 <34177126+memset0@users.noreply.github.com>
2021-07-31 22:21:31 +0800

    feat: search

--- backend/spider.py
+++ backend/spider.py
@@ -1,6 +1,13 @@
 from baiduspider import BaiduSpider
+
 baiduspider = BaiduSpider()
 
+
 def search(keyword):
-    results = baiduspider.search_web(query=keyword)['results']
-    return [result for result in results if result['type'] == 'result']
+    res = []
+    for page in range(2):
+        rsp = baiduspider.search_web(query=keyword,
+                                     exclude=['all'],
+                                     pn=page + 1)
+        res.extend([result for result in rsp['results'] if result['type'] == 'result'])
+    return res
